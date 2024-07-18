2fb087f506e3c8f2510262f75d46d761e7157978
komalle0booth <komalle0@chicagobooth.edu>
2024-07-18 05:00:20 +0000

    Adds moives #index and movies#show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,16 @@
 class MoviesController < ApplicationController
   def index
-    render({ :template => "director_templates/list"})
+    render({ :template => "movie_templates/list"})
   end
 
   def show
     the_id = params.fetch("the_id")
 
-    matching_records = Director.where({:id => the_id})
+    matching_records = Movie.where({:id => the_id})
 
-    @the_director = matching_records.at(0)
+    @the_movie = matching_records.at(0)
 
-    render({ :template => "director_templates/details"})
+    render({ :template => "movie_templates/details"})
   end
 
 end
